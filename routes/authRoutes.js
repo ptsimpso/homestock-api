5d8e6f10963baa8089a65c76bb68d66408586c32
Peter Simpson <ptaylorsimpson@gmail.com>
2020-04-20 15:53:23 -0700

    Fixes user path

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const User = require('../models/user')
+const User = require('../models/User')
 const requireLogin = require('../middleware/requireLogin')
 
 const router = new express.Router()
