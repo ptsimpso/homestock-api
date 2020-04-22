f3938c46bdb6cc8927846e3088ca37841ada5040
Peter Simpson <ptaylorsimpson@gmail.com>
2020-04-21 23:47:33 -0700

    Adds all home routes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -23,49 +23,51 @@ router.post('/homes', requireLogin, async (req, res) => {
 
   const home = new Home({
     name,
-    owner: user.id,
+    owner: user._id,
     joinCode,
-    users: [user.id],
+    users: [user._id],
     items: []
   })
 
   try {
     const savedHome = await home.save()
-    res.send(savedHome)
+    const populatedHome = await savedHome.populate('users', '_id name').execPopulate()
+    res.send(populatedHome)
   } catch (error) {
-    res.status(422).send(error)
+    res.status(500).send(error)
   }
 
 });
 
-// Read homes attached to user that are not deleted
+// Read all homes
 router.get('/homes', requireLogin, async (req, res) => {
   const { user } = req
 
   try {
     const homes = await Home.find({
-      users: user.id,
+      users: user._id,
       deleted: false
-    }).lean()
+    }).populate('users', '_id name').lean()
 
-    res.send(homes) // TODO: Send back home with populated users with only user id and names
+    res.send(homes)
   } catch (error) {
     res.status(500).send(error)
   }
 });
 
-// Read a home that is not deleted
-router.get('/homes/fetch/:id', requireLogin, async (req, res) => {
+// Read a home
+router.get('/homes/:id', requireLogin, async (req, res) => {
   try {
-    const home = await Home.findOne({ _id: req.params.id, users: req.user.id }).lean()
+    const home = await Home.findOne({ _id: req.params.id, users: req.user._id }).populate('users', '_id name').lean()
+    if (!home) return res.status(404).send({ error: 'No home found.' })
 
-    res.send(home) // TODO: Send back home with populated users with only user id and names
+    res.send(home)
   } catch (error) {
-    res.status(400).send({ error: 'ID not found.' })
+    res.status(500).send(error)
   }
 });
 
-// Join a home. Return the home.
+// Join a home
 router.post('/homes/join', requireLogin, async (req, res) => {
   
   const { user } = req
@@ -78,29 +80,55 @@ router.post('/homes/join', requireLogin, async (req, res) => {
 
   try {
     const home = await Home.findOne({ joinCode, deleted: false })
+    if (!home) return res.status(404).send({ error: 'No home found.' })
 
-    if (!home) {
-      return res.status(400).send({ error: 'No home found.' })
-    }
-
-    if (home.users.includes(user.id)) {
-      return res.status(400).send({ error: 'You have already joined this home.' })
+    if (home.users.includes(user._id)) {
+      return res.status(422).send({ error: 'You have already joined this home.' })
     }
 
     home.users.push(user)
     const updatedHome = await home.save()
-    res.send(updatedHome) // TODO: Send back home with populated users with only user id and names
+    const populatedHome = await updatedHome.populate('users', '_id name').execPopulate()
+    res.send(populatedHome)
 
   } catch (error) {
-    res.status(400).send(error)
+    res.status(500).send(error)
   }
 
 });
 
 // Update a home
+router.patch('/homes/:id', requireLogin, async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'joinCode']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+  if (!isValidOperation) return res.status(400).send('Invalid updates.')
 
+  const { id } = req.params
 
+  try {
+    const home = await Home.findOne({ _id: id, owner: req.user._id })
+    if (!home) return res.status(404).send({ error: 'No home found or you are not the owner.' })
+
+    updates.forEach((update) => home[update] = req.body[update])
+    await home.save()
+
+    res.send(home)
+  } catch (e) {
+    res.status(500).send()
+  }
+
+})
 
 // Delete a home
+router.delete('/homes/:id', requireLogin, async (req, res) => {
+  try {
+    await Home.updateOne({ _id: req.params.id, owner: req.user._id }, { deleted: true })
+    res.send()
+  } catch (error) {
+    res.status(400).send({ error: 'Home not found.' })
+  }
+});
+
 
 module.exports = router
