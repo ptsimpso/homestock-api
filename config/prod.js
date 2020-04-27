c84b0adf541d5ce92d0eb174473585395f17ff4e
Peter Simpson <ptaylorsimpson@gmail.com>
2020-04-27 16:42:40 -0700

    Adds prod aws keys

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,7 @@
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
+  AWS_ID: process.env.AWS_ID,
+  AWS_KEY: process.env.AWS_KEY,
+  AWS_S3_BUCKET: process.env.AWS_S3_BUCKET,
 }
